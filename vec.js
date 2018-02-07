6b541847bca417bca3f03912413db34fab481581
carlalara97 <carla.lara.compte@gmail.com>
2018-02-07 10:47:54 +0100

    Add files via upload

---
+++ vec.js
@@ -0,0 +1,5 @@
+var mesos = ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre"];
+
+for (var i = 0; i < 12; ++i) {
+	console.log(mesos[i]);
+}
